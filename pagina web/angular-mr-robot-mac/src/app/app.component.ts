32bed9d798850c8aaa84e60c0f15fe943193a069
manuelitod <mr.1311223@gmail.com>
2018-11-28 18:48:39 -0400

    Añadido componente scoreboard

--- pagina web/angular-mr-robot-mac/src/app/app.component.ts
+++ pagina web/angular-mr-robot-mac/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-mr-robot-mac';
+  title = 'Tarea 3 MR-ROBOT-MAC';
 }
